82d2932513232271f19771e36be7c653018bb5e9
felipesalazar24 <fe.salazarv@duocuc.cl>
2025-06-07 10:40:22 -0400

    a

--- test/src/main/java/Print3D/test/services/DataLoader.java
+++ test/src/main/java/Print3D/test/services/DataLoader.java
@@ -11,8 +11,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
-
-
+@Profile("dev_244")
+@Component
 public class DataLoader implements CommandLineRunner{
 
     @Autowired
@@ -22,20 +22,22 @@ public class DataLoader implements CommandLineRunner{
     public void run(String... args)throws Exception{
         
         Faker faker = new Faker();
-        Random random = new Random();
+        //Random random = new Random();
 
         for(int i =0; i < 10; i++){
             Usuario usuario = new Usuario();
-            usuario.setRut(faker.name().fristName());
-            usuario.setNombre(faker.code().nombre());
-            usuario.setApellido(faker.code().apellido);
-            usuario.setEmail(faker.code().email);
-            usuario.setTelefono(faker.code().telefono);
-            usuario.setDireccion(faker.code().direccion);
-            usuario.setUsuario(faker.code().nomusuario);
-            usuario.setContrasenia(faker.code().contrasenia);
-            usuario.setRol(faker.code().rol);
-            usuario.setResenias(faker.code().resenias);
+            usuario.setRut(faker.idNumber().valid());
+            usuario.setNombre(faker.name().firstName());
+            usuario.setApellido(faker.name().lastName());
+            usuario.setEmail(faker.internet().emailAddress());
+            usuario.setTelefono(faker.number().numberBetween(100000000, 999999999));
+            usuario.setDireccion(faker.address().fullAddress());
+            usuario.setUsuario(faker.name().username());
+            usuario.setContrasenia(faker.internet().password());
+            String rol = faker.options().option("creador","cliente");
+            usuario.setRol(rol);
+            String resenia = faker.lorem().sentence();
+            usuario.setResenias(resenia);
 
             System.out.println("Generando Usuario" + usuario);
 
---
+++ test/src/test/java/Print3D/test/ApplicationTest.java
@@ -0,0 +1,44 @@
+package Print3D.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+import Print3D.test.controller.UsusarioController;
+
+// @SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class TestApplication {
+	@Autowired
+	private UsusarioController ususarioController;
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	void contextLoads(){
+		System.out.println("Testing the context loading ...");
+		// System.out.println("Server running on port: " + port);
+	}
+
+	@Test
+	void contextLoads2() throws Exception{
+		System.out.println("Testing the context loading. and the controller ...");
+		assertThat(ususarioController).isNotNull();
+	}
+
+	@Test
+	void getUsuariosContainsBrackets() throws Exception{
+		assertThat(this.restTemplate.getForObject("http://localhost" + port + "/api/v1/usuarios",
+		responseType_String.class)).toString().contains("[");
+	}
+
+}
