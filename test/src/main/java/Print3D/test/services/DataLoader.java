e9462a656b5cdd86e5b6e371e1bdc648fa0a492d
felipesalazar24 <fe.salazarv@duocuc.cl>
2025-06-07 09:28:04 -0400

    a

--- test/src/main/java/Print3D/test/services/DataLoader.java
+++ test/src/main/java/Print3D/test/services/DataLoader.java
@@ -1,49 +1,49 @@
-// package Print3D.test.services;
+package Print3D.test.services;
 
-// import Print3D.test.model.Usuario;
-// import Print3D.test.repository.UsuarioRepository;
-// import net.datafaker.Faker;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.CommandLineRunner;
-// import org.springframework.context.annotation.Profile;
-// import org.springframework.stereotype.Component;
-// import java.util.Date;
-// import java.util.List;
-// import java.util.Random;
+import Print3D.test.model.Usuario;
+import Print3D.test.repository.UsuarioRepository;
+import net.datafaker.Faker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 
 
-// public class DataLoader implements CommandLineRunner{
+public class DataLoader implements CommandLineRunner{
 
-//     @Autowired
-//     private UsuarioRepository usuarioRepository;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
-//     @Override
-//     public void run(String... args)throws Exception{
+    @Override
+    public void run(String... args)throws Exception{
         
-//         Faker faker = new Faker();
-//         Random random = new Random();
+        Faker faker = new Faker();
+        Random random = new Random();
 
-//         for(int i =0; i < 10; i++){
-//             Usuario usuario = new Usuario();
-//             usuario.setRut(faker.name().fristName());
-//             usuario.setNombre(faker.code().nombre());
-//             usuario.setApellido(faker.code().apellido);
-//             usuario.setEmail(faker.code().email);
-//             usuario.setTelefono(faker.code().telefono);
-//             usuario.setDireccion(faker.code().direccion);
-//             usuario.setUsuario(faker.code().nomusuario);
-//             usuario.setContrasenia(faker.code().contrasenia);
-//             usuario.setRol(faker.code().rol);
-//             usuario.setResenias(faker.code().resenias);
+        for(int i =0; i < 10; i++){
+            Usuario usuario = new Usuario();
+            usuario.setRut(faker.name().fristName());
+            usuario.setNombre(faker.code().nombre());
+            usuario.setApellido(faker.code().apellido);
+            usuario.setEmail(faker.code().email);
+            usuario.setTelefono(faker.code().telefono);
+            usuario.setDireccion(faker.code().direccion);
+            usuario.setUsuario(faker.code().nomusuario);
+            usuario.setContrasenia(faker.code().contrasenia);
+            usuario.setRol(faker.code().rol);
+            usuario.setResenias(faker.code().resenias);
 
-//             System.out.println("Generando Usuario" + usuario);
+            System.out.println("Generando Usuario" + usuario);
 
-//             try {
-//                 usuarioRepository.save(usuario);
-//             } catch (Exception e) {
-//                 // TODO: handle exception
-//             }
-//         }
-//     }
-// }
+            try {
+                usuarioRepository.save(usuario);
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+    }
+}
